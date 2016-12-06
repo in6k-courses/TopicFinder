57fabc5f98683e44d358f37f4409f51d6d3fc921
Semenov-Yevhenii <yevheniis@softwareplanet.uk.com>
2016-12-06 18:02:21 +0200

    Configure project for using spring boot
         - add parent in pom.xml
         - add spring-boot-starter-web dependencies  in pom.xml
         - add project.build.sourceEncoding, start-class and java.version properties  in pom.xml
         - create Application class for start spring-boot
         - create MainController class

---
+++ api/src/main/java/server/Application.java
@@ -0,0 +1,14 @@
+package server; /**
+ * Created by employee on 12/6/16.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
