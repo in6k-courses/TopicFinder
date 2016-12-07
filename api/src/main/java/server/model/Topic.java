a18d0b152a4ae0282fdce7fda9acdedfd94645a9
Semenov-Yevhenii <yevheniis@softwareplanet.uk.com>
2016-12-07 08:58:06 +0200

    Fix problem with spring boot config from application.properties and add topic model
         - change spring-boot version to 1.3.5
         - add spring-boot-starter-data-jpa and mysql-connector-java dependencies
         - add Topic model class

---
+++ api/src/main/java/server/model/Topic.java
@@ -0,0 +1,51 @@
+package server.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by employee on 12/6/16.
+ */
+@Entity
+@Table(name="topic")
+public class Topic {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 3, max = 80)
+    private String title;
+
+    @NotNull
+    @Size(min = 2, max = 80)
+    private String description;
+
+    public Topic() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
