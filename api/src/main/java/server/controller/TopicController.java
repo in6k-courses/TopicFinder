b845d70a8588bdc1bca1c82f924768219230b6ba
Semenov-Yevhenii <yevheniis@softwareplanet.uk.com>
2016-12-07 11:23:00 +0200

    Api server return topics json
         - rename MainController to TopicController
         - add method viewAllTopics (return Topics json in response body)
         - add method createTopic to TopicController
         - create TopicDao witch extends from CrudRepository
         - add new constructor to Topic model with title and description parameter
         - add jackson-databind dependency

---
+++ api/src/main/java/server/controller/TopicController.java
@@ -0,0 +1,45 @@
+package server.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import server.dao.TopicDao;
+import server.model.Topic;
+
+import java.util.List;
+
+/**
+ * Created by employee on 12/6/16.
+ */
+
+@Controller
+@RequestMapping("/api/topic")
+public class TopicController {
+    @Autowired
+    private TopicDao topicDao;
+
+    @RequestMapping("/all")
+    @ResponseBody
+    public String viewAllTopics() {
+        ObjectMapper mapper = new ObjectMapper();
+        List<Topic> topic = (List<Topic>) topicDao.findAll();
+        String jsonInString = null;
+        try {
+            jsonInString = mapper.writeValueAsString(topic);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return jsonInString;
+    }
+
+    @RequestMapping("/create")
+    @ResponseBody
+    public String createTopic(String title, String description) {
+        Topic topic = new Topic("Topic title", "Description");
+        topicDao.save(topic);
+        return "";
+    }
+
+}
---
+++ api/src/main/java/server/dao/TopicDao.java
@@ -0,0 +1,15 @@
+package server.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import javax.transaction.Transactional;
+import server.model.Topic;
+
+import java.util.List;
+
+/**
+ * Created by employee on 12/7/16.
+ */
+@Transactional
+public interface TopicDao extends CrudRepository<Topic, Long> {
+
+}
