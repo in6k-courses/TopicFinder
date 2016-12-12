6c5dab401ea73ce9779cabef1997e848910dc350
Semenov-Yevhenii <yevheniis@softwareplanet.uk.com>
2016-12-12 14:58:48 +0200

    Start create topic menu and topic filter in client.

---
+++ client/src/app/page/topic/filter/topic.filter.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by yevheniis on 12/12/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'topic-filter'
+})
+export class TopicFilterComponent {
+
+}
---
+++ client/src/app/page/topic/menu/topic.top.menu.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by yevheniis on 12/12/16.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'topic-top-menu',
+  templateUrl: './topic.top.menu.component.html'
+})
+export class TopicTopMenuComponent {
+
+}
